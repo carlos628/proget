d624291e36d05d5fc0e42cf12fae7d3ce99194ce
carlos <chumberto628@gmail.com>
2015-06-16 21:25:47 -0300

    AULA

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php ?>
+
+
+<html>
+
+    <title>camisetaria</title>
+
+    <head>
+
+
+
+
+    </head>
+    <body>
+<div class="col-md-5">
+                    <form class="form-inline campo_busca">
+                        <div class="form-group">
+                            <label class="control-label">Buscar</label>
+                            <input type="text" class="form-control" placeholder="Digite uma busca">
+                        </div>
+    </body>
